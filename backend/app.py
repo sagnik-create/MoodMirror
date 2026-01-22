05fa23477d1aa7b3d66f0a13393a397201f6f325
Sagnik Dey <sagnikd587@gmail.com>
2026-01-23 00:21:02 +0530

    Fixed CORS for production

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Emotion Detection API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://mood-mirror-sand.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],                      # allow all HTTP methods
     allow_headers=["*"],                      # allow all headers
